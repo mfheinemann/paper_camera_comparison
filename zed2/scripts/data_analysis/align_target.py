7660345c1d4b200f36cadc7c3d420b1be2d61567
Jonas Herzfeld <jonas.herzfeld@cml.fraunhofer.de>
2022-01-25 11:56:56 +0100

    Adjust scripts for zed2 for video capture

--- zed2/scripts/data_analysis/align_target.py
+++ zed2/scripts/data_analysis/align_target.py
@@ -27,7 +27,7 @@ def main():
     # Define target
     shape   = 'rectangle'
     if shape == 'rectangle':
-        center  = np.array([[0.0], [0.0], [1.0]])    # Center of plane
+        center  = np.array([[0.1], [0.05], [1.0]])    # Center of plane
         size    = np.array([0.5, 0.5])               # (width, height) in m
         angle   = 0.0                                # In radiants
     elif shape == 'circle':
--- zed2/scripts/data_collection/save_point_cloud.py
+++ zed2/scripts/data_collection/save_point_cloud.py
@@ -1,15 +1,39 @@
 import pyzed.sl as sl
-import datetime
 import numpy as np
 import cv2
+import os
+import sys
+import tkinter as tk
+from tkinter import messagebox
 
 def main():
-    show_image  = True
-    depth_fps   = 30
-    sim_seconds = 5
+    DURATION = 25                # measurement duration
+    LOG_PATH = '../../logs/log_zed'
+    RS_MODEL = '2'
+    NAME = '1'           # name of the files
+    DEPTH_RES = [1280, 720]  # desired depth resolution
+    DEPTH_RATE = 30         # desired depth frame rate
+    COLOR_RES = [1280, 720]  # desired rgb resolution
+    COLOR_RATE = 30         # desired rgb frame rate
+    num_frames = DURATION * DEPTH_RATE
+
+    color_path = LOG_PATH + RS_MODEL + '_' + NAME + '_rgb.avi'
+    depth_path = LOG_PATH + RS_MODEL + '_' + NAME + '_depth.avi'
+    depth_array_path = LOG_PATH + RS_MODEL + '_' + NAME + '_pc'
+    colorwriter = cv2.VideoWriter(color_path, cv2.VideoWriter_fourcc(*'XVID'), COLOR_RATE, (COLOR_RES[0], COLOR_RES[1]), 1)
+    depthwriter = cv2.VideoWriter(depth_path, cv2.VideoWriter_fourcc(*'XVID'), DEPTH_RATE, (DEPTH_RES[0], DEPTH_RES[1]), 1)
+
+    if os.path.exists(depth_array_path + '.npz'):
+        root = tk.Tk()
+        root.withdraw() 
+        if messagebox.askokcancel("Exit", "File already exists! Overwrite ?"):
+            print("Overwriting!")
+        else:
+            print("Canceling!")
+            sys.exit()
 
     init = sl.InitParameters(camera_resolution = sl.RESOLUTION.HD720,
-                                 camera_fps = depth_fps,
+                                 camera_fps = DEPTH_RATE,
                                  depth_mode = sl.DEPTH_MODE.ULTRA,
                                  coordinate_units = sl.UNIT.MILLIMETER,
                                  coordinate_system = sl.COORDINATE_SYSTEM.RIGHT_HANDED_Y_UP)
@@ -26,47 +50,47 @@ def main():
 
     depth_image = sl.Mat()
     depth_map  = sl.Mat()
-    frames = []
-    intrinsic_params = []
-    extrinsic_params = []
+    rgb_image = sl.Mat()
+    extrinsic_params_array = np.zeros((num_frames, 3, 4), dtype=np.float64)
+    intrinsic_params_array = np.zeros((num_frames, 3, 3), dtype=np.float64)
+    frames_array = np.zeros((num_frames,DEPTH_RES[1], DEPTH_RES[0], 4), dtype=np.uint16)
 
-    num_frames = sim_seconds * depth_fps
     for i in range(num_frames):
         print("Frame: " + str(i + 1))
         if zed.grab(runtime_params) == sl.ERROR_CODE.SUCCESS:
             zed.retrieve_measure(depth_map, sl.MEASURE.XYZ, sl.MEM.CPU)
-            cloud = depth_map.get_data()
+            zed.retrieve_image(depth_image, sl.VIEW.DEPTH, sl.MEM.CPU)
+            zed.retrieve_image(rgb_image, sl.VIEW.LEFT, sl.MEM.CPU)
 
-            if show_image:
-                zed.retrieve_image(depth_image, sl.VIEW.DEPTH)
-                image = depth_image.get_data()
-                cv2.imshow("ZED | image", image)
-                cv2.waitKey(1)
+            cloud = depth_map.get_data()
+            depth_frame = depth_image.get_data()
+            color_frame = rgb_image.get_data()
             
+            colorwriter.write(color_frame[:,:,:-1])
+            depthwriter.write(depth_frame[:,:,:-1])
             cloud_uint16 = np.uint16(cloud)
-            frames.append(cloud_uint16)
 
             K = np.array([[cam_params.left_cam.fx, 0, cam_params.left_cam.cx],
               [0, cam_params.left_cam.fy, cam_params.left_cam.cy],
               [0, 0, 1]])
-            intrinsic_params.append(K)
 
             R = pose.get_rotation_matrix(sl.Rotation()).r.T     
             t = pose.get_translation(sl.Translation()).get()            
             extrinsic_matrix = np.concatenate((R, np.array([t]).T), axis=1)
-            extrinsic_params.append(extrinsic_matrix)
 
+            extrinsic_params_array[i,:,:] = extrinsic_matrix
+            intrinsic_params_array[i,:,:] = K
+            frames_array[i,:,:,:] = cloud_uint16
 
+            cv2.imshow("ZED | rgb_image", color_frame)
+            cv2.waitKey(1)
+
+    colorwriter.release()
+    depthwriter.release()
     cv2.destroyAllWindows()
     zed.close()
 
-    extrinsic_params_array = np.stack(extrinsic_params, axis=0)
-    intrinsic_params_array = np.stack(intrinsic_params, axis=0)
-    frames_array = np.stack(frames, axis=0)
-
-    current_datetime = datetime.datetime.now()
-    video_title = "../../logs/log_zed2_" + current_datetime.strftime("%y%m%d-%H%M%S")
-    np.savez_compressed(video_title, data=frames_array, 
+    np.savez_compressed(depth_array_path, data=frames_array, 
                         intrinsic_params=intrinsic_params_array, 
                         extrinsic_params=extrinsic_params_array)
 
