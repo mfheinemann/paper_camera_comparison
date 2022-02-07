f364d671f6acb1b9f73ba892a0542a1527bacb6d
Jonas Herzfeld <jonas.herzfeld@cml.fraunhofer.de>
2022-02-07 15:11:48 +0100

    Adjust Oak-D scripts to enable data collection

--- oak-d/scripts/data_analysis/align_target.py
+++ oak-d/scripts/data_analysis/align_target.py
@@ -5,100 +5,115 @@ import numpy as np
 import depthai as dai
 from crop_target.crop_target import CropTarget
 
-resolutionMap = {"800": (1280, 800), "720": (1280, 720), "400": (640, 400)}
-resolution = resolutionMap["720"]
+def main():
+    print("Check that target is aligned in the frame... Press 'q' to exit")
 
-outRectified = False  # Output and display rectified streams
-lrcheck = False  # Better handling for occlusions
-extended = False  # Closer-in minimum depth, disparity range is doubled
-subpixel = False  # Better accuracy for longer distance, fractional disparity 32-levels
+    # Define target
+    shape   = 'rectangle'
+    if shape == 'rectangle':
+        center  = np.array([[0.0], [0.0], [3.985]])    # Center of plane
+        size    = np.array([0.5, 0.5])               # (width, height) in m
+        angle   = np.radians(0.0)                      # In degrees                           # In radiants
+    elif shape == 'circle':
+        center  = np.array([[1.0], [0.0], [3.0]])   # Center of shpere
+        size    = 0.2                               # Radius in m
+        angle   = 0.0
+    else:
+        print("Not a valid shape!")
+    edge_width = 0
+    target  = CropTarget(shape, center, size, angle, edge_width)
 
-medianMap = {
-    "OFF": dai.StereoDepthProperties.MedianFilter.MEDIAN_OFF,
-    "3x3": dai.StereoDepthProperties.MedianFilter.KERNEL_3x3,
-    "5x5": dai.StereoDepthProperties.MedianFilter.KERNEL_5x5,
-    "7x7": dai.StereoDepthProperties.MedianFilter.KERNEL_7x7,
-}
-median = medianMap["7x7"]
-
-
-def getDisparityFrame(frame):
-    maxDisp = stereo.initialConfig.getMaxDisparity()
-    disp = (frame * (255.0 / maxDisp)).astype(np.uint8)
-
-    return disp
-
-
-print("Creating Stereo Depth pipeline")
-pipeline = dai.Pipeline()
-
-camLeft = pipeline.create(dai.node.MonoCamera)
-camRight = pipeline.create(dai.node.MonoCamera)
-stereo = pipeline.create(dai.node.StereoDepth)
-xoutDisparity = pipeline.create(dai.node.XLinkOut)
-
-camLeft.setBoardSocket(dai.CameraBoardSocket.LEFT)
-camRight.setBoardSocket(dai.CameraBoardSocket.RIGHT)
-res = (
-    dai.MonoCameraProperties.SensorResolution.THE_800_P
-    if resolution[1] == 800
-    else dai.MonoCameraProperties.SensorResolution.THE_720_P
-    if resolution[1] == 720
-    else dai.MonoCameraProperties.SensorResolution.THE_400_P
-)
-for monoCam in (camLeft, camRight):  # Common config
-    monoCam.setResolution(res)
-    # monoCam.setFps(20.0)
-
-stereo.setDefaultProfilePreset(dai.node.StereoDepth.PresetMode.HIGH_DENSITY)
-stereo.initialConfig.setMedianFilter(median)  # KERNEL_7x7 default
-stereo.setRectifyEdgeFillColor(0)  # Black, to better see the cutout
-stereo.setLeftRightCheck(lrcheck)
-stereo.setExtendedDisparity(extended)
-stereo.setSubpixel(subpixel)
-
-xoutDisparity.setStreamName("disparity")
-
-camLeft.out.link(stereo.left)
-camRight.out.link(stereo.right)
-stereo.disparity.link(xoutDisparity.input)
-
-streams = ["disparity", "depth"]
+    lrcheck      = False  # Better handling for occlusions
+    extended     = False  # Closer-in minimum depth, disparity range is doubled
+    subpixel     = False  # Better accuracy for longer distance, fractional disparity 32-levels
+    resolution   = (1280, 720)
+    medianMap    = {
+        "OFF": dai.StereoDepthProperties.MedianFilter.MEDIAN_OFF,
+        "3x3": dai.StereoDepthProperties.MedianFilter.KERNEL_3x3,
+        "5x5": dai.StereoDepthProperties.MedianFilter.KERNEL_5x5,
+        "7x7": dai.StereoDepthProperties.MedianFilter.KERNEL_7x7,
+    }
+    median = medianMap["7x7"]
 
 
-# Define target
-shape   = 'rectangle'
-if shape == 'rectangle':
-    center  = np.array([[1.0], [0.0], [2.0]])    # Center of plane
-    size    = np.array([0.5, 0.5])               # (width, height) in m
-    angle   = 0.9                                # In radiants
-elif shape == 'circle':
-    center  = np.array([[1.0], [0.0], [3.0]])   # Center of shpere
-    size    = 0.2                               # Radius in m
-    angle   = 0.0
-else:
-    print("Not a valid shape!")
-edge_width = 0
-target  = CropTarget(shape, center, size, angle, edge_width)
+    def getDisparityFrame(frame):
+        maxDisp = stereo.initialConfig.getMaxDisparity()
+        disp = (frame * (255.0 / maxDisp)).astype(np.uint8)
+        disp = cv2.applyColorMap(disp, cv2.COLORMAP_JET)
 
-print("Creating DepthAI device")
-with dai.Device(pipeline) as device:
-    # Create a receive queue for each stream
-    disp_queue = device.getOutputQueue("disparity", 4, blocking=False)
-    calibData = device.readCalibration()
+        return disp
 
-    while True:
-        disp_frame = disp_queue.get().getCvFrame()  # blocking call, will wait until a new data has arrived
-        disp_frame = getDisparityFrame(disp_frame)
 
-        intrinsic_params = np.array(calibData.getCameraIntrinsics(dai.CameraBoardSocket.LEFT, resolution[0], resolution[1]))
+    print("Creating Stereo Depth pipeline")
+    pipeline = dai.Pipeline()
 
-        R = np.array([[1, 0, 0],[0, 1, 0],[0, 0, 1]])
-        t = np.zeros((3,1))         
-        extrinsic_params = np.concatenate((R, t), axis=1)
+    camLeft        = pipeline.create(dai.node.MonoCamera)
+    camRight       = pipeline.create(dai.node.MonoCamera)
+    camRgb         = pipeline.create(dai.node.ColorCamera)
+    stereo         = pipeline.create(dai.node.StereoDepth)
+    xoutDisparity  = pipeline.create(dai.node.XLinkOut)
+    xoutDepth      = pipeline.create(dai.node.XLinkOut)
+    xoutRgb        = pipeline.create(dai.node.XLinkOut)
+
+    camLeft.setBoardSocket(dai.CameraBoardSocket.LEFT)
+    camRight.setBoardSocket(dai.CameraBoardSocket.RIGHT)
+    res = dai.MonoCameraProperties.SensorResolution.THE_720_P
+
+    for monoCam in (camLeft, camRight):  # Common config
+        monoCam.setResolution(res)
+        monoCam.setFps(30.0)
+
+    stereo.setDefaultProfilePreset(dai.node.StereoDepth.PresetMode.HIGH_DENSITY)
+    stereo.initialConfig.setMedianFilter(median)  # KERNEL_7x7 default
+    stereo.setRectifyEdgeFillColor(0)  # Black, to better see the cutout
+    stereo.setLeftRightCheck(lrcheck)
+    stereo.setExtendedDisparity(extended)
+    stereo.setSubpixel(subpixel)
+    camRgb.setInterleaved(False)
+    camRgb.setColorOrder(dai.ColorCameraProperties.ColorOrder.RGB)
+    camRgb.setPreviewSize(1280, 720)
+
+    xoutDisparity.setStreamName("disparity")
+    xoutDepth.setStreamName("depth")
+    xoutRgb.setStreamName("rgb")
+
+    camLeft.out.link(stereo.left)
+    camRight.out.link(stereo.right)
+    stereo.disparity.link(xoutDisparity.input)
+    stereo.depth.link(xoutDepth.input)
+    camRgb.preview.link(xoutRgb.input)
+
+    with dai.Device(pipeline) as device:
+        # Create a receive queue for each stream
+        depth_queue = device.getOutputQueue("depth", 4, blocking=False)
+        disp_queue = device.getOutputQueue("disparity", 4, blocking=False)
+        rgb_queue = device.getOutputQueue(name="rgb", maxSize=4, blocking=False)
     
-        image_with_target = target.crop_to_target(disp_frame, extrinsic_params, intrinsic_params)
-        cv2.imshow("OAK-D | image", image_with_target)
+        while True:
+            depth_frame = depth_queue.get().getCvFrame().astype(np.uint16)  # blocking call, will wait until a new data has arrived
+            disp_frame = disp_queue.get().getCvFrame()  # blocking call, will wait until a new data has arrived
+            disp_frame = getDisparityFrame(disp_frame)
+            rbg_frame = rgb_queue.get().getCvFrame()  # blocking call, will wait until a new data has arrived
 
-        if cv2.waitKey(1) == ord("q"):
-            break
+
+            calibData = device.readCalibration()
+            intrinsic_matrix = np.array(calibData.getCameraIntrinsics(dai.CameraBoardSocket.LEFT, 1280, 720))
+            extrinsic_matrix = np.hstack((np.identity(3), np.zeros((3,1))))
+            
+            #extrinsic_matrix = np.array(calibData.getCameraExtrinsics(dai.CameraBoardSocket.LEFT, dai.CameraBoardSocket.RGB))
+            #extrinsic_matrix = extrinsic_matrix[:-1,:]
+            #extrinsic_matrix[:,-1] = extrinsic_matrix[:,-1] / 100 # Translation is in cm for some reason
+
+
+            image_mask = target.crop_to_target(rbg_frame, extrinsic_matrix, intrinsic_matrix)
+            image_with_target = target.show_target_in_image(rbg_frame, extrinsic_matrix, intrinsic_matrix)
+
+            #image_concat = np.vstack((image_with_target, image_mask))
+    
+            cv2.imshow("disparity", image_with_target)
+            if cv2.waitKey(1) == ord("q"):
+                break
+
+
+if __name__ == "__main__":
+    main()
--- oak-d/scripts/data_collection/save_depth_map.py
+++ oak-d/scripts/data_collection/save_depth_map.py
@@ -1,85 +1,140 @@
 #!/usr/bin/env python3
 
-import cv2
 import numpy as np
 import depthai as dai
+import cv2
+import os
+import sys
+import tkinter as tk
+from tkinter import messagebox
+from matplotlib import pyplot as plt
 
-resolutionMap = {"800": (1280, 800), "720": (1280, 720), "400": (640, 400)}
-resolution = resolutionMap["720"]
+def main():
+    DURATION = 1                # measurement duration
+    LOG_PATH = '../../logs/log_oak-d'
+    NAME = '0'           # name of the files
+    DEPTH_RES = [1280, 720]  # desired depth resolution
+    DEPTH_RATE = 30         # desired depth frame rate
+    COLOR_RES = [1280, 720]  # desired rgb resolution
+    COLOR_RATE = 30         # desired rgb frame rate
+    num_frames = DURATION * DEPTH_RATE
 
-outRectified = False  # Output and display rectified streams
-lrcheck = False  # Better handling for occlusions
-extended = False  # Closer-in minimum depth, disparity range is doubled
-subpixel = False  # Better accuracy for longer distance, fractional disparity 32-levels
+    color_path = LOG_PATH + '_' + NAME + '_rgb.avi'
+    depth_path = LOG_PATH + '_' + NAME + '_depth.avi'
+    depth_array_path = LOG_PATH + '_' + NAME + '_pc'
+    colorwriter = cv2.VideoWriter(color_path, cv2.VideoWriter_fourcc(*'XVID'), COLOR_RATE, (COLOR_RES[0], COLOR_RES[1]), 1)
+    depthwriter = cv2.VideoWriter(depth_path, cv2.VideoWriter_fourcc(*'XVID'), DEPTH_RATE, (DEPTH_RES[0], DEPTH_RES[1]), 1)
 
-medianMap = {
-    "OFF": dai.StereoDepthProperties.MedianFilter.MEDIAN_OFF,
-    "3x3": dai.StereoDepthProperties.MedianFilter.KERNEL_3x3,
-    "5x5": dai.StereoDepthProperties.MedianFilter.KERNEL_5x5,
-    "7x7": dai.StereoDepthProperties.MedianFilter.KERNEL_7x7,
-}
-median = medianMap["7x7"]
+    if os.path.exists(depth_array_path + '.npz'):
+        root = tk.Tk()
+        root.withdraw() 
+        if messagebox.askokcancel("Exit", "File already exists! Overwrite ?"):
+            print("Overwriting!")
+        else:
+            print("Canceling!")
+            sys.exit()
+
+    lrcheck      = False  # Better handling for occlusions
+    extended     = False  # Closer-in minimum depth, disparity range is doubled
+    subpixel     = False  # Better accuracy for longer distance, fractional disparity 32-levels
+    resolution   = (1280, 720)
+    medianMap    = {
+        "OFF": dai.StereoDepthProperties.MedianFilter.MEDIAN_OFF,
+        "3x3": dai.StereoDepthProperties.MedianFilter.KERNEL_3x3,
+        "5x5": dai.StereoDepthProperties.MedianFilter.KERNEL_5x5,
+        "7x7": dai.StereoDepthProperties.MedianFilter.KERNEL_7x7,
+    }
+    median = medianMap["7x7"]
 
 
-def getDisparityFrame(frame):
-    maxDisp = stereo.initialConfig.getMaxDisparity()
-    disp = (frame * (255.0 / maxDisp)).astype(np.uint8)
-    #disp = cv2.applyColorMap(disp, cv2.COLORMAP_JET)
+    def getDisparityFrame(frame):
+        maxDisp = stereo.initialConfig.getMaxDisparity()
+        disp = (frame * (255.0 / maxDisp)).astype(np.uint8)
+        disp = cv2.applyColorMap(disp, cv2.COLORMAP_JET)
 
-    return disp
+        return disp
 
 
-print("Creating Stereo Depth pipeline")
-pipeline = dai.Pipeline()
+    print("Creating Stereo Depth pipeline")
+    pipeline = dai.Pipeline()
 
-camLeft = pipeline.create(dai.node.MonoCamera)
-camRight = pipeline.create(dai.node.MonoCamera)
-stereo = pipeline.create(dai.node.StereoDepth)
-xoutDisparity = pipeline.create(dai.node.XLinkOut)
-xoutDepth = pipeline.create(dai.node.XLinkOut)
+    camLeft        = pipeline.create(dai.node.MonoCamera)
+    camRight       = pipeline.create(dai.node.MonoCamera)
+    camRgb         = pipeline.create(dai.node.ColorCamera)
+    stereo         = pipeline.create(dai.node.StereoDepth)
+    xoutDisparity  = pipeline.create(dai.node.XLinkOut)
+    xoutDepth      = pipeline.create(dai.node.XLinkOut)
+    xoutRgb        = pipeline.create(dai.node.XLinkOut)
 
-camLeft.setBoardSocket(dai.CameraBoardSocket.LEFT)
-camRight.setBoardSocket(dai.CameraBoardSocket.RIGHT)
-res = (
-    dai.MonoCameraProperties.SensorResolution.THE_800_P
-    if resolution[1] == 800
-    else dai.MonoCameraProperties.SensorResolution.THE_720_P
-    if resolution[1] == 720
-    else dai.MonoCameraProperties.SensorResolution.THE_400_P
-)
-for monoCam in (camLeft, camRight):  # Common config
-    monoCam.setResolution(res)
-    # monoCam.setFps(20.0)
+    camLeft.setBoardSocket(dai.CameraBoardSocket.LEFT)
+    camRight.setBoardSocket(dai.CameraBoardSocket.RIGHT)
+    res = dai.MonoCameraProperties.SensorResolution.THE_720_P
 
-stereo.setDefaultProfilePreset(dai.node.StereoDepth.PresetMode.HIGH_DENSITY)
-stereo.initialConfig.setMedianFilter(median)  # KERNEL_7x7 default
-stereo.setRectifyEdgeFillColor(0)  # Black, to better see the cutout
-stereo.setLeftRightCheck(lrcheck)
-stereo.setExtendedDisparity(extended)
-stereo.setSubpixel(subpixel)
+    for monoCam in (camLeft, camRight):  # Common config
+        monoCam.setResolution(res)
+        monoCam.setFps(30.0)
 
-xoutDisparity.setStreamName("disparity")
-xoutDepth.setStreamName("depth")
+    stereo.setDefaultProfilePreset(dai.node.StereoDepth.PresetMode.HIGH_DENSITY)
+    stereo.initialConfig.setMedianFilter(median)  # KERNEL_7x7 default
+    stereo.setRectifyEdgeFillColor(0)  # Black, to better see the cutout
+    stereo.setLeftRightCheck(lrcheck)
+    stereo.setExtendedDisparity(extended)
+    stereo.setSubpixel(subpixel)
+    camRgb.setInterleaved(False)
+    camRgb.setColorOrder(dai.ColorCameraProperties.ColorOrder.RGB)
+    camRgb.setPreviewSize(1280, 720)
 
-camLeft.out.link(stereo.left)
-camRight.out.link(stereo.right)
-stereo.disparity.link(xoutDisparity.input)
-stereo.depth.link(xoutDepth.input)
+    xoutDisparity.setStreamName("disparity")
+    xoutDepth.setStreamName("depth")
+    xoutRgb.setStreamName("rgb")
 
-streams = ["disparity", "depth"]
+    camLeft.out.link(stereo.left)
+    camRight.out.link(stereo.right)
+    stereo.disparity.link(xoutDisparity.input)
+    stereo.depth.link(xoutDepth.input)
+    camRgb.preview.link(xoutRgb.input)
 
-print("Creating DepthAI device")
-with dai.Device(pipeline) as device:
-    # Create a receive queue for each stream
-    depth_queue = device.getOutputQueue("depth", 4, blocking=False)
-    disp_queue = device.getOutputQueue("disparity", 4, blocking=False)
+    extrinsic_params_array = np.zeros((num_frames, 3, 4), dtype=np.float64)
+    intrinsic_params_array = np.zeros((num_frames, 3, 3), dtype=np.float64)
+    frames_array = np.zeros((num_frames,resolution[1], resolution[0]), dtype=np.uint16)
+    with dai.Device(pipeline) as device:
+        # Create a receive queue for each stream
+        depth_queue = device.getOutputQueue("depth", 4, blocking=False)
+        disp_queue = device.getOutputQueue("disparity", 4, blocking=False)
+        rgb_queue = device.getOutputQueue(name="rgb", maxSize=4, blocking=False)
+    
+        for i in range(num_frames):
+            print("Frame: " + str(i + 1))
+            depth_frame = depth_queue.get().getCvFrame().astype(np.uint16)  # blocking call, will wait until a new data has arrived
+            disp_frame = disp_queue.get().getCvFrame()  # blocking call, will wait until a new data has arrived
+            disp_frame = getDisparityFrame(disp_frame)
+            rbg_frame = rgb_queue.get().getCvFrame()  # blocking call, will wait until a new data has arrived
 
-    while True:
-        depth_frame = depth_queue.get().getCvFrame().astype(np.uint16)  # blocking call, will wait until a new data has arrived
-        disp_frame = disp_queue.get().getCvFrame()  # blocking call, will wait until a new data has arrived
-        disp_frame = getDisparityFrame(disp_frame)
+            colorwriter.write(rbg_frame)
+            depthwriter.write(disp_frame)
 
-        cv2.imshow("disparity", disp_frame)
+            calibData = device.readCalibration()
+            intrinsic_matrix = np.array(calibData.getCameraIntrinsics(dai.CameraBoardSocket.LEFT, 1280, 720))
+            #extrinsic_matrix = np.array(calibData.getCameraExtrinsics(dai.CameraBoardSocket.LEFT, dai.CameraBoardSocket.RGB))
+            #extrinsic_matrix[:,-1] = extrinsic_matrix[:,-1] / 100   # Translation is in cm for some reason
 
-        if cv2.waitKey(1) == ord("q"):
-            break
+
+            extrinsic_params_array[i,:,:] = np.hstack((np.identity(3), np.zeros((3,1))))
+            intrinsic_params_array[i,:,:] = intrinsic_matrix
+            frames_array[i,:,:] = depth_frame.astype(np.int16)
+    
+            cv2.imshow("disparity", rbg_frame)
+            if cv2.waitKey(1) == ord("q"):
+                break
+
+
+    colorwriter.release()
+    depthwriter.release()
+    cv2.destroyAllWindows()
+    np.savez_compressed(depth_array_path, data=frames_array, 
+                        intrinsic_params=intrinsic_params_array, 
+                        extrinsic_params=extrinsic_params_array)
+
+
+if __name__ == "__main__":
+    main()
