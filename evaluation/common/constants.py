9835d66d67e42d2d0f692f3bd296df33ef1ca640
Jonas Herzfeld <jonas.herzfeld@cml.fraunhofer.de>
2022-02-09 14:04:59 +0100

    Add constants file

---
+++ evaluation/common/constants.py
@@ -0,0 +1,13 @@
+## Constants for evaluation
+# Target definition
+TARGET_SIZE     = (0.5, 0.5)
+REDUCE_TARGET   = 0.02
+SPHERE_RADIUS   = 0.139 / 2.0
+
+# Offset of camera sensor to mounting position
+OFFSET = {'rs435'   : 0.015, 
+          'rs455'   : 0.015,
+          'zed2'    : 0.015,
+          'orbbec'  : 0.015,
+          'oak'     : 0.015,
+          'oakpro'  : 0.015,}
--- evaluation/setup_1_bias.py
+++ evaluation/setup_1_bias.py
@@ -2,19 +2,20 @@
 # calulate bias, precision and edge precision from depth data
 
 import numpy as np
-import math as m
 import cv2
 import tkinter as tk
 from tkinter import filedialog
 from crop_target.crop_target import CropTarget
-import edge_precision.edge_precision as ep
+from edge_precision.edge_precision import *
+from common.constants import *
+
 
 # Define target
 shape   = 'rectangle'
-center  = np.array([[0.0], [0.0], [1.488]])    # Center of plane
-size    = np.array([0.48, 0.48])               # (width, height) in m
-angle   = np.radians(0.0)                      # In degrees
-edge_width = 40
+center  = np.array([[0.0], [0.0], [1.0 - OFFSET['zed2']]])
+size    = np.asarray(TARGET_SIZE) - REDUCE_TARGET
+angle   = 0.0
+edge_width = 10
 target  = CropTarget(shape, center, size, angle, edge_width)
 
 
@@ -56,8 +57,8 @@ def main():
         precision[i] = np.std(image_cropped[mask_bool])
 
         # Correct target to real size to get edges
-        target.size = np.array([0.50, 0.50]) 
-        edge_precision[i, :] = ep.get_edge_precision(target, depth_image, mean_depth, extrinsic_params, intrinsic_params)
+        target.size = np.asarray(TARGET_SIZE)
+        edge_precision[i, :] = get_edge_precision(target, depth_image, mean_depth, extrinsic_params, intrinsic_params)
         target.size = size        
     
     total_bias = np.mean(bias)
